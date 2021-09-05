2ab62d23f8c3da2bd085c11760d99fd6db05a422
zhenekns <35662589+zhenekq@users.noreply.github.com>
2021-09-05 21:03:29 +0300

    added navbar and admin security VIEW

--- src/main/java/by/zhenekns/dev/iba/controller/MainController.java
+++ src/main/java/by/zhenekns/dev/iba/controller/MainController.java
@@ -4,23 +4,27 @@ import by.zhenekns.dev.iba.model.Message;
 import by.zhenekns.dev.iba.model.User;
 import by.zhenekns.dev.iba.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
 
     @Autowired
     private MessageRepository messageRepository;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting(Map<String, Object> model) {
@@ -29,12 +33,12 @@ public class MainController {
 
     @GetMapping("/main")
     public String main(@RequestParam(required = false, defaultValue = "") String filter,
-                       Map<String, Object> model){
+                       Map<String, Object> model) {
         Iterable<Message> messages = messageRepository.findAll();
 
-        if(filter != null && !filter.isEmpty()){
+        if (filter != null && !filter.isEmpty()) {
             messages = messageRepository.findByTag(filter);
-        }else{
+        } else {
             messages = messageRepository.findAll();
         }
 
@@ -48,8 +52,21 @@ public class MainController {
                       @RequestParam String text,
                       @RequestParam String tag,
                       @RequestParam("file") MultipartFile file,
-                      Map<String, Object> model){
+                      Map<String, Object> model) throws IOException {
         Message message = new Message(text, tag, user);
+
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+            message.setFilename(resultFilename);
+        }
+
         messageRepository.save(message);
 
         Iterable<Message> messages = messageRepository.findAll();
